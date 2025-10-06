c5927047dab0af970f5a0a9ec74d385efb1c7e2a
burgessleia1 <burgessleia1@gmail.com>
2025-10-06 16:58:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let currentDate = new Date().toLocaleDateString("en-US", {
   year: "numeric",
   month: "long",
-  day: "numeric"
+  day: "numeric",
 });
+
+document.getElementById("lastUpdated").textContent = currentDate;
+
 // Example: log when a project is clicked
-document.querySelectorAll("a").forEach(link => {
+document.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", () => {
     console.log(`Opening: ${link.textContent}`);
   });
 });
+
